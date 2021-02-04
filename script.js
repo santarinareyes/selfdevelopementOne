44a4fba2a4468ea9b7b36d5ee8722969106ad983
Richard <richard.santarinareyes@medieinstitutet.se>
2021-02-04 22:10:22 +0100

    New attempt, a lot faster, still clean

--- script.js
+++ script.js
@@ -86,6 +86,10 @@
 //   document.querySelector("body").style.backgroundColor = "#FFFFFF";
 // }
 
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
 //// SECOND ATTEMPT ////
 
 // START: 2021-02-04 : 16.06
@@ -107,60 +111,140 @@
 // New guessing number
 // input value to null
 
-///////////////////////////////////////////////////////
-// Variables
+// ///////////////////////////////////////////////////////
+// // Variables
+// let random = document.querySelector(".random").value;
+// random = Math.trunc(Math.random() * 10 + 1);
+
+// const background = document.querySelector("body");
+// const btnGuess = document.querySelector(".guess");
+// const btnReset = document.querySelector(".reset");
+// let currentScore = 10;
+// let highscore = 0;
+// ///////////////////////////////////////////////////////
+// // Listeners
+// btnGuess.addEventListener("click", guessClicked);
+// btnReset.addEventListener("click", resetClicked);
+
+// ///////////////////////////////////////////////////////
+// // Functions
+// function guessClicked() {
+//   const theGuess = Number(document.querySelector(".number").value);
+
+//   if (!theGuess) {
+//     alterText("Please write a number");
+//   } else if (theGuess === random) {
+//     alterText("Correct!");
+//     newHighscore();
+//     newBackground();
+//     document.querySelector(".random").textContent = theGuess;
+//   } else if (theGuess !== random && currentScore > 1) {
+//     alterText(theGuess > random ? "Guess is too high" : "Guess is too low");
+//     currentScore--;
+//     newScore();
+//   } else {
+//     currentScore--;
+//     newScore();
+//     newBackground();
+//     alterText("You lost!");
+//   }
+// }
+
+// function alterText(newText) {
+//   document.querySelector(".texts").textContent = newText;
+// }
+
+// function newScore() {
+//   if (currentScore >= 0)
+//     document.querySelector(".score").textContent = currentScore;
+// }
+
+// function newHighscore() {
+//   document.querySelector(".highscore").textContent = currentScore;
+// }
+
+// function newBackground() {
+//   if (currentScore > 0) {
+//     background.style.backgroundColor = "#008000";
+//   } else {
+//     background.style.backgroundColor = "#FF0000";
+//   }
+// }
+
+// function resetClicked() {
+//   currentScore = 10;
+//   random = Math.trunc(Math.random() * 10 + 1);
+//   document.querySelector(".score").textContent = currentScore;
+//   background.style.backgroundColor = "#FFFFFF";
+//   document.querySelector(".number").value = "";
+// }
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+//// THIRD ATTEMPT ////
+
+// START: 2021-02-04 : 21.45
+// Goal: Faster, Cleaner(?)
+// DONE: 2021-02-04 : 22.06
+// Achieved? = YEEEEEEEES!!!!!
+
+//Variables
 let random = document.querySelector(".random").value;
 random = Math.trunc(Math.random() * 10 + 1);
 
-const background = document.querySelector("body");
 const btnGuess = document.querySelector(".guess");
+const texts = document.querySelector(".texts");
+
+let score = document.querySelector(".score");
+score.textContent = 10;
+
+const highscore = document.querySelector(".highscore");
+highscore.textContent = 0;
+
+const background = document.querySelector("body");
+
 const btnReset = document.querySelector(".reset");
-let currentScore = 10;
-let highscore = 0;
-///////////////////////////////////////////////////////
-// Listeners
+
+//Listeners
 btnGuess.addEventListener("click", guessClicked);
 btnReset.addEventListener("click", resetClicked);
 
-///////////////////////////////////////////////////////
-// Functions
+//Functions
 function guessClicked() {
-  const theGuess = Number(document.querySelector(".number").value);
-
-  if (!theGuess) {
-    alterText("Please write a number");
-  } else if (theGuess === random) {
-    alterText("Correct!");
+  const newGuess = Number(document.querySelector(".number").value);
+  if (!newGuess) {
+    newText("Please write a numbers");
+  } else if (newGuess === random) {
+    newText("Good job");
     newHighscore();
-    newBackground();
-    document.querySelector(".random").textContent = theGuess;
-  } else if (theGuess !== random && currentScore > 1) {
-    alterText(theGuess > random ? "Guess is too high" : "Guess is too low");
-    currentScore--;
+    newBody();
+    document.querySelector(".random").textContent = random;
+  } else if (score.textContent > 1) {
+    newText(newGuess > random ? "Guess is too high" : "Guess is too low");
     newScore();
   } else {
-    currentScore--;
-    newScore();
-    newBackground();
-    alterText("You lost!");
+    score.textContent = 0;
+    newText("You lost!");
+    newBody();
   }
 }
 
-function alterText(newText) {
-  document.querySelector(".texts").textContent = newText;
+function newText(text) {
+  texts.textContent = text;
 }
 
 function newScore() {
-  if (currentScore >= 0)
-    document.querySelector(".score").textContent = currentScore;
+  score.textContent--;
 }
 
 function newHighscore() {
-  document.querySelector(".highscore").textContent = currentScore;
+  highscore.textContent = score.textContent;
 }
 
-function newBackground() {
-  if (currentScore > 0) {
+function newBody() {
+  if (score.textContent > 1) {
     background.style.backgroundColor = "#008000";
   } else {
     background.style.backgroundColor = "#FF0000";
@@ -168,9 +252,8 @@ function newBackground() {
 }
 
 function resetClicked() {
-  currentScore = 10;
   random = Math.trunc(Math.random() * 10 + 1);
-  document.querySelector(".score").textContent = currentScore;
-  background.style.backgroundColor = "#FFFFFF";
+  score.textContent = 10;
   document.querySelector(".number").value = "";
+  background.style.backgroundColor = "#FFFFFF";
 }
